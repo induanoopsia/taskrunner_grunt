caaa243d942465f0a7dcae853fe0e684ac13773c
induanoopsia <121377462+induanoopsia@users.noreply.github.com>
2023-07-02 23:27:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Select button by id
-    const MyButton = document.getElementById('clickit');
+    var MyButton = document.getElementById('clickit');
     //Add on click listener for button
     MyButton.addEventListener('click', function() {
         //Select (h1) heading by id, and then change it's value to (bananas)
         document.getElementById('display').innerText = "bananas"
-    })
+    });
