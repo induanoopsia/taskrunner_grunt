265a51a54b1b5071a032262fe53557029c329f79
induanoopsia <121377462+induanoopsia@users.noreply.github.com>
2023-07-02 22:55:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function(){
+$(function(){
 	document.getElementById("clickit").onclick = function(){
 		var display = document.getElementById("display");
 		display.innerText = "Hello";
 	};
-}
+});
